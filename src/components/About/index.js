6891d2b73b773c258388a296f34c61922db25b02
trieule50 <trieule50@gmail.com>
2021-06-25 12:02:36 -0400

    Add bootstrap

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -2,6 +2,10 @@ export default function About(){
     return (
         <div className='about'>
             <h1>ABOUT</h1>
+            <div>
+                <p>Having knowledge on nutrition, not only help with maintaining a healthy weight but it also can help reduce the risk of some disease. <br></br>Nutritious Recipes is an app that allows anyone to search up recipes based on their dietary needs or their choice in ingredients. <br></br>Nutritious Recipes uses data from Edamam API. For more information regarding the API, please go to the Edamam website.</p>
+            </div>
+            <div id="edamam-badge" data-color="white"></div>
         </div>
     )
 }
