fa044d6a95daea1292901b44ef59bfa08e7f4e02
trieule50 <trieule50@gmail.com>
2021-06-28 17:44:35 -0400

    Individual Recipe Made

--- src/App.js
+++ src/App.js
@@ -8,97 +8,47 @@ import About from './components/About';
 import Search from './components/Search';
 import Recipe from './components/Recipe';
 
-const sampleData = [
-  {
-    label: "Frothy Iced Matcha Green Tea Recipe",
-    image: "https://www.edamam.com/web-img/643/643d4bad9cc21284f7f52b1b9b862848.jpg",
-    url: "http://www.seriouseats.com/recipes/2016/08/iced-matcha-green-tea-recipe.html",
-    ingredientLines: [
-      "2 teaspoons (6g) Japanese matcha green tea (see note above)",
-      "8 ounces (235ml) cold water"
-    ],
-    dietLabels: [
-      "High-Protein",
-      "Low-Fat"
-    ],
-    calories: 0.06,
-    yield: 2
-  },
-  {
-    label: "Corn Tortilla",
-    image: "https://www.edamam.com/web-img/3a1/3a134b216e935d9866a57e4d6c4e742d.jpg",
-    url: "http://norecipes.com/blog/2009/07/23/how-to-make-tortillas/",
-    ingredientLines: [
-      "2 C masa harina",
-      "1/2 tsp salt kosher salt (less if using table salt)",
-      "1 1/4 C warm water"
-    ],
-    dietLabels: [
-      "Low-Fat"
-    ],
-    calories: 827.6400000000001,
-    yield: 4
-  },
-  {
-    label: "Chopstick Ready Rice",
-    image: "https://www.edamam.com/web-img/099/0995e680f24efa8d1b369326ed889eb6.jpg",
-    url: "https://www.epicurious.com/recipes/food/views/chopstick-ready-rice-51240420",
-    ingredientLines:[
-      "2 cups shortgrain white rice",
-      "2 cups water"
-    ],
-    dietLabels: [
-      "Low-Fat"
-    ],
-    calories: 1404,
-    yield: 8
-  },
-  {
-    label: "Annatto Oil Recipe",
-    image: "https://www.edamam.com/web-img/b5a/b5a5553cd6b6b72968466a9a5db0f80b.jpg",
-    url: "https://www.chowhound.com/recipes/annatto-oil-10483",
-    ingredientLines:[
-      "1/2 cup annatto seeds",
-      "2 cups mild olive oil or other vegetable oil"
-    ],
-    dietLabels: [
-      "Low-Carb"
-    ],
-    calories: 4105.704000004849,
-    yield: 12
-  },
-  {
-    label: "Pub-style pork scratchings",
-    image: "https://www.edamam.com/web-img/6c7/6c764aaddab053b8a962b44474cb735d.jpg",
-    url: "http://www.jamieoliver.com/recipes/pork-recipes/pub-style-pork-scratchings/",
-    ingredientLines: [
-      "1/2 cup annatto seeds",
-      "2 cups mild olive oil or other vegetable oil"
-    ],
-    dietLabels: [
-      "Low-Carb"
-    ],
-    calories: 4105.704000004849,
-    yield: 8
-  },
-  {
-    label: "Fresh Corn Tortillas recipes",
-    image: "https://www.edamam.com/web-img/1a7/1a7b1689c96d91d2b891aa3ba40f34fd",
-    url: "http://www.bonappetit.com/recipe/fresh-corn-tortillas",
-    ingredientLines:[
-      "2 cups (or more) masa (corn tortilla mix; preferably Maseca brand)",
-      "1/2 teaspoon kosher salt",
-      "vegetable oil"
-    ],
-    dietLabels: [
-      "Low-Fat"
-    ],
-    calories: 855.3428656666815,
-    yield: 24
-  }
-]
-
 function App() {
+
+  const [searchString, setSearchString] = useState('chicken');
+    const [searchHealth, setSearchHealth] = useState('low-sugar');
+    const [recipes, setRecipes] = useState([]);
+
+    const handleChange = (event) =>{
+        setSearchString(event.target.value);
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        getApiData(searchString);
+
+    }
+
+    const searchOptions = {
+        key: process.env.REACT_APP_EDAMAM_KEY,
+        id: process.env.REACT_APP_EDAMAM_ID,
+        api: 'https://api.edamam.com/api/recipes/v2?type=public&'
+    }
+
+    const getApiData = async () =>{
+        const apiEndPoint = `${searchOptions.api}q=${searchString}&app_id=${searchOptions.id}&app_key=${searchOptions.key}&health=${searchHealth}`;
+        try{
+            const response = await fetch(apiEndPoint, {
+                mode: 'cors'
+            });
+            const data = await response.json();
+            // console.log(data);
+            setRecipes(data.hits);
+        }catch(error){
+            console.log(error)
+        }
+    }
+
+    // console.log(recipes);
+
+    useEffect(()=>{
+        getApiData();
+    },[])
 
   return (
     <div className="App">
@@ -106,8 +56,8 @@ function App() {
       <main>
         <Route path="/" exact render={()=> <Home/> }/>
         <Route path="/about" exact render={()=> <About /> }/>
-        <Route path="/search" exact render={()=> <Search />}/>
-        <Route path="/recipe/:label" exact render={()=> <Recipe sampleData={sampleData}/> }/>
+        <Route path="/search" exact render={()=> <Search searchString={searchString} recipes={recipes} handleSubmit={handleSubmit} handleChange={handleChange}/>}/>
+        <Route path="/recipe/:recipe:label" exact render={( routeProps )=> <Recipe {...routeProps} searchOptions={searchOptions} searchHealth={searchHealth}/> }/>
       </main>
     </div>
   );
--- src/components/Recipe/index.js
+++ src/components/Recipe/index.js
@@ -1,39 +1,61 @@
 import { useState, useEffect } from 'react';
 
-export default function Recipe({ sampleData}){
+export default function Recipe( routeProps ){
+    // console.log(routeProps)
+
+    const [uniqueRecipe, setUniqueRecipe] = useState([]);
+    const name = routeProps.match.params.recipe+routeProps.match.params.label;
+    // const { recipe } = routeProps.recipes
+    // console.log(name);
+    // console.log(routeProps.searchOptions); 
+
+    const getApiData = async () =>{
+        const apiEndPoint = `${routeProps.searchOptions.api}q=${name}&app_id=${routeProps.searchOptions.id}&app_key=${routeProps.searchOptions.key}&health=${routeProps.searchHealth}`
+        try{
+            const response = await fetch(apiEndPoint, {
+                mode: 'cors'
+            });
+            const data = await response.json();
+            // console.log(data);
+            setUniqueRecipe(data.hits[0].recipe);
+        }catch(error){
+            console.log(error)
+        }
+    }
+
+    console.log(uniqueRecipe);
+
+    useEffect(()=>{
+        getApiData();
+    },[])
+
     return (
         <div>
-            {sampleData.map((data, i) =>{
-                return(
-                    <div key={i}>
-                        <h1>{data.label}</h1>
-                        <img src={data.image} alt={data.label} />
-                        <p>This recipe can be found <a href={data.url}target='_blank'>here.</a></p>
-                        <ul className='ingredients'>
-                            <h4>Ingredients:</h4>
-                            {data.ingredientLines.map((ingredient, i)=>{
-                            return(
-                                <li key={i}>
-                                    {ingredient}
-                                </li>
-                            )
-                            })}
-                        </ul>
-                        <ul className="diet-labels">
-                            <h4>Diet Labels:</h4>
-                            {data.dietLabels.map((label, i)=>{
-                                return(
-                                    <li key={i}>
-                                        {label}
-                                    </li>
-                                )
-                            })}
-                        </ul>
-                        <p>Calories: {data.calories.toFixed(2)}</p>
-                        <p>This recipe yield: {data.yield}</p>
-                    </div>
-                )
-            })}
+                <h1>{name}</h1>
+                <img src={uniqueRecipe.image} alt={uniqueRecipe.label} />
+                <p>This recipe can be found <a href={uniqueRecipe.url}target='_blank'>here.</a></p>
+                <ul className='ingredients'>
+                <h4>Ingredients:</h4>
+                    {uniqueRecipe.ingredientLines.map((ingredient, i)=>{
+                        return(
+                            <li key={i}>
+                                {ingredient}
+                            </li>
+                        )
+                    })}
+                </ul>
+                <ul className="diet-labels">
+                <h4>Diet Labels:</h4>
+                    {uniqueRecipe.dietLabels.map((label, i)=>{
+                        return(
+                            <li key={i}>
+                                    {label}
+                            </li>
+                        )
+                    })}
+                </ul>
+                <p>Calories: {uniqueRecipe.calories.toFixed(2)}</p>
+                <p>This recipe yield: {uniqueRecipe.yield}</p>
         </div>
     )
 }
